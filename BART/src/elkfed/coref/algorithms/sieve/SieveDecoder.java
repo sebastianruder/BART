561d24296b1259242f9350555cb8ef280b010218
ruder <ruder.sebastian@gmail.com>
2014-05-16 14:58:25 +0200

    Added switch, cursory process
    SieveDecoder now goes through all mentions 10 times, selects different
    sieve based on number of walkthrough with switch statement; could maybe
    be optimized by using a SieveFactory

--- BART/src/elkfed/coref/algorithms/sieve/SieveDecoder.java
+++ BART/src/elkfed/coref/algorithms/sieve/SieveDecoder.java
@@ -2,8 +2,11 @@ package elkfed.coref.algorithms.sieve;
 
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import net.cscott.jutil.DisjointSet;
+import elkfed.config.ConfigProperties;
 import elkfed.coref.CorefResolver;
 import elkfed.coref.eval.LinkScorer;
 import elkfed.coref.eval.SplitLinkScorer;
@@ -16,14 +19,82 @@ import elkfed.coref.mentions.Mention;
 */
 
 public class SieveDecoder implements CorefResolver {
-	
+	protected static final Logger _logger = Logger.getAnonymousLogger();
 	protected LinkScorer _scorer = new SplitLinkScorer();
-
-	@Override
+	
 	public DisjointSet<Mention> decodeDocument(List<Mention> mentions,
 			Map<Mention, Mention> antecedents) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		// creates data structure where disjoint sets of mentions are
+		// going to be stored
+		DisjointSet<Mention> clusters = new DisjointSet<Mention>();
+		// counts number of links
+		int numLinks = 0;		
+        _logger.log(Level.INFO,
+                String.format("%s: decode document with %d mentions\n",
+                getClass().getSimpleName(),
+                mentions.size()));        
+        
+        // counts number of walk_throughs
+        
+        for (int walk_through = 1; walk_through < 11; walk_through++) {
+		    
+		    for (int i = 0; i < mentions.size(); i++) {
+		    	
+		    	/* Instead of the switch statement, it would be better just being
+		    	 * able to pass the number of the walk_through and get an instance
+		    	 * of the appropriate sieve pack
+		    	 * Maybe implement a SieveFactory
+		    	 * with method createSieve(int walkthrough);
+		    	 * 
+		    	 */
+		    	
+		    	switch(walk_through) {
+		    	case 1: SpeakerIdentificatonSieve;
+		    	// puts singletons in a single disjoint set
+				// --> confirm with Yannick
+		    	if (ConfigProperties.getInstance().getOutputSingletons()) 
+		            clusters.union(mentions.get(i), mentions.get(i));
+		    	case 2: StringMatchSieve;
+		    	case 3: RelaxedStringMatchSieve;
+		    	case 4: PreciseConstructSieve;
+		    	case 5: StrictHeadMatchASieve;
+		    	case 6: StrictHeadMatchBSieve;
+		    	case 7: StrictHeadMatchCSieve;
+		    	case 8: ProperHeadNounMatchSieve;
+		    	case 9: RelaxedHeadMatchSieve;
+		    	case 10: PronounMatchSieve;
+		    	}
+		    	
+		    	// return of resolveSingle method is either -1 or index of coreferent
+		    	int ante_idx = -1; 
+		    	
+		        //int ante_idx=resolveSingle(mentions,i);
+		        
+		    	if (ante_idx==-1)
+		        {
+		           _scorer.scoreNonlink(mentions,i); 
+		        }
+		        else
+		        {
+		            numLinks++;
+		            clusters.union(mentions.get(i),mentions.get(ante_idx));
+		            antecedents.put(mentions.get(i), mentions.get(ante_idx));
+		            mentions.get(i).linkToAntecedent(mentions.get(ante_idx));
+		            _scorer.scoreLink(mentions, ante_idx, i);
+		            if (_logger.isLoggable(Level.FINE)) {
+		                Object[] args={mentions.get(i),mentions.get(ante_idx)};
+		                    _logger.log(Level.FINE,
+		                                "joining %s and %s\n",
+		                                args);
+		            }
+		        }
+		    }
+        }
+        _logger.log(Level.INFO,String.format("joined %d pairs in %d mentions",
+                numLinks,mentions.size()));
+	    //_scorer.displayResults();
+	    return clusters;
 	}
 
     public void printStatistics() {
