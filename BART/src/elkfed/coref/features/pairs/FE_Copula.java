b9ed9bcadd3f7b9776fd020fb9749d580c09f0ed
remu5 <ruder.sebastian@gmail.com>
2014-06-12 15:36:08 +0200

    Cleaned up FE_Copula code

--- BART/src/elkfed/coref/features/pairs/FE_Copula.java
+++ BART/src/elkfed/coref/features/pairs/FE_Copula.java
@@ -35,7 +35,7 @@ import elkfed.knowledge.SemanticClass;
 
 /**
  * Uses parse trees, not surface regexp!
- * @author olga
+ * @author olga (cleaned up by sebastian)
  */
 public class FE_Copula implements PairFeatureExtractor {
 
@@ -51,89 +51,116 @@ public class FE_Copula implements PairFeatureExtractor {
     }
 
     public static Boolean getCopula(PairInstance inst) {
-
-// should be in the same sentence
-      if (inst.getAnaphor().getSentId()!=inst.getAntecedent().getSentId()) 
-           return false;
-
-
-// should have not-null  maxnp-trees (otherwise -- problematic mentions)
-
-      Tree sentenceTree=inst.getAnaphor().getSentenceTree();
-      Tree AnaTree=inst.getAnaphor().getMaxNPParseTree();
-      Tree AnteTree=inst.getAntecedent().getMaxNPParseTree();
-      if (sentenceTree==null) return false;
-      if (AnaTree==null) return false;
-      if (AnteTree==null) return false;
-
-// exclude "there is .." (ToDo: exclude other expletives!)
-
-      if (inst.getAntecedent().getMarkableString().toLowerCase().matches(NONREF_NP)) return false;
-
-//exclude date and time
-
-if (inst.getAnaphor().getSemanticClass()==SemanticClass.TIME) return false;
-if (inst.getAnaphor().getSemanticClass()==SemanticClass.DATE) return false;
-if (inst.getAntecedent().getSemanticClass()==SemanticClass.TIME) return false;
-if (inst.getAntecedent().getSemanticClass()==SemanticClass.DATE) return false;
-
-
-// should be subj-obj of the same verb
-
-      Tree vp=AnaTree.parent(sentenceTree);
-      if (vp==null) return false;
-      if (!vp.value().equalsIgnoreCase("vp")) return false;
-      while(vp.parent(sentenceTree)!=null && vp.parent(sentenceTree).value().equalsIgnoreCase("vp")) {vp=vp.parent(sentenceTree);}
-      if (vp==null) return false;
-      if (vp.parent(sentenceTree)==null) return false;
-
-      Boolean foundante=false;
-      Tree[] chldsup=vp.parent(sentenceTree).children();
-      for (int i=0; i<chldsup.length; i++) {
-       if (chldsup[i]==AnteTree) foundante=true;
-       if (chldsup[i]==vp && foundante==false) return false;
+   
+    // should be in the same sentence
+      if (inst.getAnaphor().getSentId()!=inst.getAntecedent().getSentId()) {
+    	  return false;
       }
-vp=AnaTree.parent(sentenceTree); // we do not want to go higher here -- "S is *ing O" fires otherwise
+           
+      // should have not-null  maxnp-trees (otherwise -- problematic mentions)
 
-// should not contain a modal verb
-      Tree[] chlds=vp.children();
-      for (int i=0; i<chlds.length; i++) {
-       if (chlds[i].value().equalsIgnoreCase("rb")) return false;
+      Tree sentenceTree = inst.getAnaphor().getSentenceTree();
+      Tree AnaTree = inst.getAnaphor().getMaxNPParseTree();
+      Tree AnteTree = inst.getAntecedent().getMaxNPParseTree();
+      if (sentenceTree == null) {
+    	  return false;
+      }
+      if (AnaTree == null) {
+    	  return false;
+      }
+      if (AnteTree == null) {
+    	  return false;
+      }
 
-       if (chlds[i].value().equalsIgnoreCase("md") && 
-         chlds[i].getLeaves().get(0).value().toLowerCase().matches(MODAL_VERB))
-            return false;
+      // exclude "there is .." (ToDo: exclude other expletives!)
 
+      if (inst.getAntecedent().getMarkableString().toLowerCase().matches(NONREF_NP)) {
+    	  return false;
+      }
+
+      //exclude date and time
+
+      if (inst.getAnaphor().getSemanticClass()==SemanticClass.TIME) {
+    	  return false;
+      }
+      if (inst.getAnaphor().getSemanticClass()==SemanticClass.DATE) {
+    	  return false;
+      }
+      if (inst.getAntecedent().getSemanticClass()==SemanticClass.TIME) {
+    	  return false;
+      }
+      if (inst.getAntecedent().getSemanticClass()==SemanticClass.DATE) {
+    	  return false;
+      }
+
+
+      // should be subj-obj of the same verb
+
+      Tree vp = AnaTree.parent(sentenceTree);
+      if (vp == null) {
+    	  return false;
+      }
+      if (!vp.value().equalsIgnoreCase("vp")) {
+    	  return false;
+      }
+      while(vp.parent(sentenceTree)!=null && vp.parent(sentenceTree).value().equalsIgnoreCase("vp")) {
+    	  vp=vp.parent(sentenceTree);
+      }
+      if (vp.parent(sentenceTree) == null) {
+    	  return false;
+      }
+
+      Boolean foundante = false;
+      Tree[] chldsup = vp.parent(sentenceTree).children();
+      for (int i = 0; i < chldsup.length; i++) {
+    	  if (chldsup[i] == AnteTree) {
+    		  foundante = true;
+    	  }
+    	  if (chldsup[i] == vp && foundante == false) {
+    	   return false;
+    	  }
+      }
+       
+      vp = AnaTree.parent(sentenceTree); // we do not want to go higher here -- "S is *ing O" fires otherwise
+
+      // should not contain a modal verb
+      Tree[] chlds = vp.children();
+      for (int i = 0; i < chlds.length; i++) {
+    	  if (chlds[i].value().equalsIgnoreCase("rb")){
+    		  return false;
+    	  }
+    	  if (chlds[i].value().equalsIgnoreCase("md") &&
+    			  chlds[i].getLeaves().get(0).value().toLowerCase().matches(MODAL_VERB)) {
+    		  return false;
+    	  }
       }
   
-// the verb should be one of the copula verbs
+      // the verb should be one of the copula verbs
 
       for (int i=0; i<chlds.length; i++) {
 
-        if (chlds[i].value().equalsIgnoreCase("vbd") ||
-         chlds[i].value().equalsIgnoreCase("aux") ||
-         chlds[i].value().equalsIgnoreCase("vbn") ||
-         chlds[i].value().equalsIgnoreCase("vb") ||
-         chlds[i].value().equalsIgnoreCase("vbd") ||
-         chlds[i].value().equalsIgnoreCase("vbp") ||
-         chlds[i].value().equalsIgnoreCase("vbz") ||
-         chlds[i].value().equalsIgnoreCase("vbg")) {
-           if (chlds[i].getLeaves().get(0).value().toLowerCase().matches(COPULA_VERB)) {
-/*
-System.out.println("Found positive copula verb (" 
- +chlds[i].getLeaves().get(0).value() +
-") for ("+
-inst.getAnaphor().getMarkableString()+
-"),("+
-inst.getAntecedent().getMarkableString()+
-") ");
-*/
-return true;
-}
-        }
+    	  if (chlds[i].value().equalsIgnoreCase("vbd") ||
+    			  chlds[i].value().equalsIgnoreCase("aux") ||
+    			  chlds[i].value().equalsIgnoreCase("vbn") ||
+    			  chlds[i].value().equalsIgnoreCase("vb") ||
+    			  chlds[i].value().equalsIgnoreCase("vbd") ||
+    			  chlds[i].value().equalsIgnoreCase("vbp") ||
+    			  chlds[i].value().equalsIgnoreCase("vbz") ||
+    			  chlds[i].value().equalsIgnoreCase("vbg")) {
+    		  if (chlds[i].getLeaves().get(0).value().toLowerCase().matches(COPULA_VERB)) {
+    			  /*
+					System.out.println("Found positive copula verb (" 
+ 					+chlds[i].getLeaves().get(0).value() +
+					") for ("+
+					inst.getAnaphor().getMarkableString()+
+					"),("+
+					inst.getAntecedent().getMarkableString()+
+					") ");
+    			  */
+    			  return true;
+    		  }
+    	  }
       }
-
       return false;
    }
-
 }
