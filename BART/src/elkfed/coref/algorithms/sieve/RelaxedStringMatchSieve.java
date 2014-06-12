1a156383d72e25b4bc470d3c73ebd841a42da232
remu5 <ruder.sebastian@gmail.com>
2014-06-12 14:02:48 +0200

    First version of RelaxedStringMatchSieve
    First impression: seems redundant (see comment)

--- BART/src/elkfed/coref/algorithms/sieve/RelaxedStringMatchSieve.java
+++ BART/src/elkfed/coref/algorithms/sieve/RelaxedStringMatchSieve.java
@@ -4,10 +4,53 @@ import java.util.List;
 
 import elkfed.coref.mentions.Mention;
 
+/**
+ * This sieve considers two nominal mentions as coreferent,
+ * if the strings obtained by dropping the text following
+ * their head words (such as relative clauses and PP and
+ * participial postmodifiers) are identical (e.g., [Clinton]
+ * and [Clinton, whose term ends in January]).
+ * 
+ * IMPORTANT: Mentions identified by BART DON'T contain
+ * such post-modifying text.
+ * 
+ * Relaxed String Match -- in this respect -- is the same
+ * as Exact String Match. Ignoring pre-modifiers -- in turn --
+ * would make it equal to Strict Head Match.
+ * Possibilities:
+ * 	- omit sieve
+ * 	- ???
+ * 
+ * @author Sebastian
+ *
+ */
+
+
 public class RelaxedStringMatchSieve extends Sieve {
 
+	private List<Mention> potentialAntecedents;
+	
 	public RelaxedStringMatchSieve(List<Mention> mentions) {
-		// TODO Auto-generated constructor stub
+		this.potentialAntecedents = mentions;			
 	}
+		
+	public int runSieve(Mention mention){
+		int mention_idx = potentialAntecedents.indexOf(mention);
+		int ante_idx = -1;
+		// sentences should be displayed somehow
+		// Markable[] array = mention.getSentenceMarkables("sentence");
+		for (int idx = 0; idx < mention_idx; idx++){
+
+			if (mention.toString().equals(potentialAntecedents.get(idx).toString())){
+				// articles are still matched; needs to be fixed
+				if (!(mention.getPronoun())) {
+					ante_idx = idx;
+				}
+			}
+		}
+		//System.out.println(potentialAntecedents.get(ante_idx));
+		return ante_idx;
+	}
+	
 
 }
