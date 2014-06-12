c90f61ad0e60582541791e0577207e4ea079e593
remu5 <ruder.sebastian@gmail.com>
2014-06-12 16:42:57 +0200

    Partly implemented methods

--- BART/src/elkfed/coref/algorithms/sieve/PreciseConstructSieve.java
+++ BART/src/elkfed/coref/algorithms/sieve/PreciseConstructSieve.java
@@ -1,13 +1,188 @@
 package elkfed.coref.algorithms.sieve;
 
+import static elkfed.lang.EnglishLinguisticConstants.ARTICLE;
+import static elkfed.lang.EnglishLinguisticConstants.DEMONSTRATIVE;
+import static elkfed.mmax.MarkableLevels.DEFAULT_POS_LEVEL;
+
 import java.util.List;
 
+import elkfed.coref.PairFeatureExtractor;
+import elkfed.coref.PairInstance;
+import elkfed.coref.features.pairs.FE_Appositive;
+import elkfed.coref.features.pairs.FE_AppositiveParse;
+import elkfed.coref.features.pairs.FE_Copula;
 import elkfed.coref.mentions.Mention;
+import static elkfed.lang.EnglishLinguisticConstants.*;
+
+/**
+ * This sieve links two mentions if any of the following
+ * conditions are satisified:
+ * 1. 	Appositive: the two nominal mentions are in an appositive
+ * 		construction (e.g., [Israel’s Deputy Defense Minister],
+ * 		[Ephraim Sneh], said . . . ).
+ * 		Appositives: third children of a parent NP whose expansion
+ * 		begins with (NP , NP), when there is not a conjunction
+ * 		in the expansion.
+ * 2. 	Predicate nominative – the two mentions (nominal or
+ * 		pronominal) are in a copulative subject–object relation
+ * 		(e.g., [The New York-based College Board] is [a nonprofit
+ * 		organization that administers the SATs and promotes
+ * 		higher education]).
+ * 3.	Role appositive – the candidate antecedent is headed by
+ * 		a noun and appears as a modifier in an NP whose head is
+ * 		the current mention (e.g., [[actress] Rebecca Schaeffer]).
+ * 		This feature matches only if: (a) the mention is labeled
+ * 		as a person, (b) the antecedent is animate, and (c) the
+ * 		antecedent’s gender is not neutral.
+ * 4. 	Relative pronoun – the mention is a relative pronoun that
+ * 		modifies the head of the antecedent NP (e.g., [the finance
+ * 		street [which] has already formed in the Waitan district]).
+ * 5.	Acronym – both mentions are tagged as NNP and one of them
+ * 		is an acronym of the other (e.g., [Agence France Presse]...
+ * 		[AFP]). A mention is an acronym of another mention if its
+ * 		text equals the sequence of upper case characters in the
+ * 		other mention.
+ * 6. 	Demonym – one of the mentions is a demonym of the other
+ * 		(e.g., [Israel]...[Israeli]). For demonym detection we use
+ * 		a static list of countries and their gentilic forms from
+ * 		Wikipedia.
+ * 
+ * @author Sebastian
+ *
+ */
 
 public class PreciseConstructSieve extends Sieve {
-
+	
+	// list of antecedents/potential coreferents
+	private List<Mention> mentions;
+	
+	
 	public PreciseConstructSieve(List<Mention> mentions) {
-		// TODO Auto-generated constructor stub
+		this.mentions = mentions;
 	}
+	
+	public int runSieve(Mention mention){
+		
+		PairInstance pair;
+		int mention_idx = mentions.indexOf(mention);
+		int ante_idx = -1;
+		
+		for (int idx = 0; idx < mention_idx; idx++){
+			
+			pair = new PairInstance(mention, mentions.get(idx));
+			if (isAppositive(pair) || isPredicateNominative(pair) || isRoleAppositive(pair) || isRelativePronoun(pair)
+					|| isAcronym(pair) || isDemonym(pair)){
+				// articles are still matched; needs to be fixed
+				if (!(mention.getPronoun())) {
+					ante_idx = idx;
+				}
+			}
+		}
+		return ante_idx;
+	}
+
+	private boolean isAppositive(PairInstance pair) {
+		/*
+		 * there is FE_AppositiveParse.getAppositivePrs(pair) as well
+		 * might have to investigate how they differ
+		 */
+		if (FE_Appositive.getAppositive(pair)) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isPredicateNominative(PairInstance pair) {
+		if (FE_Copula.getCopula(pair)){
+			return true;
+		}
+		return false;
+	}		
+	
+	private boolean isRoleAppositive(PairInstance pair) {
+		/*
+		 * there is FE_AnimacyAgree
+		 * FE_AnimacyAgree() doesn't seem to be implemented, though
+		 */
+		if (pair.getAnaphor().getProperName() && // check if person
+				isAnimate(pair.getAntecedent()) && // check if animate 
+				!isNeutral(pair.getAntecedent())) { // check if neutral
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isAnimate(Mention mention) {
+		/**
+		 * Animacy is set using:
+		 * (a) a static list for pronouns;
+		 * (b) NER labels (e.g., PERSON is animate whereas LOCATION is not); and
+		 * (c) a dictionary bootstrapped from the Web (Ji and Lin 2009)
+		 */
+		// (a) check with pronoun list
+		// not necessary for isRoleAppositive, maybe for other applications
+		String[] tokens = mention.getMarkable().getDiscourseElements();
+		for (int i = 0; i < tokens.length; i++) {
+			String t = tokens[i].toLowerCase();
+			if (t.matches(SINGULAR_PRONOUN_ADJ) || t.matches(PLURAL_PRONOUN_ADJ)) {
+				return true;
+			}
+		}
+		// (b) check with NER labels
+		
+		// (c) check with bootstrapped dictionary
+		
+		return false;
+	}
+	
+	private boolean isNeutral(Mention mention) {
+		String[] tokens = mention.getMarkable().getDiscourseElements();
+		for (int i = 0; i < tokens.length; i++) {
+			String t = tokens[i].toLowerCase();
+			/* 
+			 * check if a (modified) noun, e.g., the marvelous actress
+			 * is neutral
+			 */
+		}
+		return false;
+	}
+
+	private boolean isRelativePronoun(PairInstance pair) {
+		
+		String[] tokens = pair.getAnaphor().getMarkable().getDiscourseElements();
+		if (tokens.length == 1 && tokens[0].matches(RELATIVE_PRONOUN)) {
+			// check if modifies head of antecedent NP
+		}
+		return false;
+	}
+	
+	private boolean isAcronym(PairInstance pair) {
+		String mention = pair.getAnaphor().toString();
+		String antecedent = pair.getAntecedent().toString();
+		// check if mention or antecedent only contain upper letters
+		
+		
+		String mention_initials = "";
+		String antecedent_initials = "";
+		for (String word : mention.split(" ")) {
+			mention_initials += word.substring(0,1).toUpperCase();
+		}
+		for (String word : antecedent.split(" ")) {
+			antecedent_initials += word.substring(0,1).toUpperCase();
+		}
+		
+		
+		return false;
+	}
+
+	private boolean isDemonym(PairInstance pair) {
+		/**
+		 * Check if one expression is a demonym of the other using:
+		 * (a) a static list of countries and their gentilic forms from
+		 * 		Wikipedia
+		 */ 		
+		
+		return false;
+	}	
 
 }
