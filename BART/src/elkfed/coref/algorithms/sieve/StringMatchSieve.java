d76570ee459827c9c6c7c1e15296a6e441131692
remu5 <ruder.sebastian@gmail.com>
2014-06-12 15:40:19 +0200

    Added alternate condition, reference for fix

--- BART/src/elkfed/coref/algorithms/sieve/StringMatchSieve.java
+++ BART/src/elkfed/coref/algorithms/sieve/StringMatchSieve.java
@@ -2,7 +2,9 @@ package elkfed.coref.algorithms.sieve;
 
 import java.util.List;
 
+import elkfed.coref.PairInstance;
 import elkfed.coref.discourse_entities.DiscourseEntity;
+import elkfed.coref.features.pairs.FE_StringMatch;
 import elkfed.coref.mentions.Mention;
 import elkfed.mmax.minidisc.Markable;
 
@@ -15,22 +17,32 @@ import elkfed.mmax.minidisc.Markable;
 public class StringMatchSieve extends Sieve {
 	
 	private List<Mention> potentialAntecedents;
-	
 		
 	StringMatchSieve(List<Mention> potentialAntecedents){
 		this.potentialAntecedents = potentialAntecedents;			
 	}
 	
-	
 	public int runSieve(Mention mention){
+		
+		FE_StringMatch fe_stringmatch = new FE_StringMatch();
+		
 		int mention_idx = potentialAntecedents.indexOf(mention);
 		int ante_idx = -1;
 		// sentences should be displayed somehow
 		// Markable[] array = mention.getSentenceMarkables("sentence");
 		for (int idx = 0; idx < mention_idx; idx++){
 
+			/*
+			 * existing StringMatch implementation of BART
+			 * finds less matches than our method, though
+			 * if (fe_stringmatch.getStringMatch(new PairInstance(mention, potentialAntecedents.get(idx))
+			 */
 			if (mention.toString().equals(potentialAntecedents.get(idx).toString())){
-				// articles are still matched; needs to be fixed
+				/*
+				 * articles are still matched; needs to be fixed
+				 * refer to getMarkableString method in FE_Pronominal_StrMatch for fix
+				 * 
+				*/				
 				if (!(mention.getPronoun())) {
 					ante_idx = idx;
 				}
