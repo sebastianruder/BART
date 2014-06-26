51429213cc5bb287a849495955b97558d2627da2
sebastianruder <ruder.sebastian@gmail.com>
2014-06-26 23:42:12 +0200

    Moved utilities from sieve in own class

--- BART/src/elkfed/coref/algorithms/sieve/PreciseConstructSieve.java
+++ BART/src/elkfed/coref/algorithms/sieve/PreciseConstructSieve.java
@@ -26,6 +26,7 @@ import elkfed.lang.GermanLanguagePlugin;
 import elkfed.lang.LanguagePlugin;
 import elkfed.lang.LanguagePlugin.TableName;
 import static elkfed.lang.EnglishLinguisticConstants.*;
+import elkfed.coref.algorithms.sieve.SieveUtilities.*;
 
 /**
  * This sieve links two mentions if any of the following
@@ -66,9 +67,10 @@ import static elkfed.lang.EnglishLinguisticConstants.*;
 
 public class PreciseConstructSieve extends Sieve {
 	
+	private static final SieveUtilities s = new SieveUtilities();
+	
 	// list of antecedents/potential coreferents
 	private List<Mention> mentions;
-	private static final LanguagePlugin langPlugin = ConfigProperties.getInstance().getLanguagePlugin();
 	
 	public PreciseConstructSieve(List<Mention> mentions) {
 		this.mentions = mentions;
@@ -80,11 +82,10 @@ public class PreciseConstructSieve extends Sieve {
 		int mention_idx = mentions.indexOf(mention);
 		int ante_idx = -1;
 		
-		for (int idx = 0; idx < mention_idx; idx++){
-			
+		for (int idx = 0; idx < mention_idx; idx++) {			
 			pair = new PairInstance(mention, mentions.get(idx));
-			if (isAppositive(pair) || isPredicateNominative(pair) || isRoleAppositive(pair) || isRelativePronoun(pair)
-					|| isAcronym(pair) || isDemonym(pair)){
+			if (s.isAppositive(pair) || s.isPredicateNominative(pair) || s.isRoleAppositive(pair) || s.isRelativePronoun(pair)
+					|| s.isAcronym(pair) || s.isDemonym(pair)){
 				// articles are still matched; needs to be fixed
 				if (!(mention.getPronoun())) {
 					ante_idx = idx;
@@ -93,155 +94,4 @@ public class PreciseConstructSieve extends Sieve {
 		}
 		return ante_idx;
 	}
-
-	private boolean isAppositive(PairInstance pair) {
-		/*
-		 * there is FE_AppositiveParse.getAppositivePrs(pair) as well
-		 * might have to investigate how they differ
-		 * 
-		 * appositive constructions as one NP in TüBa-D/Z
-		 * switch on and off
-		 * 
-		 * there is also FE_Appositive.getAppositive(pair)
-		 */
-		if (FE_AppositiveParse.getAppositivePrs(pair)) {
-			return true;
-		}
-		return false;
-	}
-
-	private boolean isPredicateNominative(PairInstance pair) {
-		if (FE_Copula.getCopula(pair)){
-			return true;
-		}
-		return false;
-	}		
-	
-	private boolean isRoleAppositive(PairInstance pair) {
-		/*
-		 * there is FE_AnimacyAgree
-		 * FE_AnimacyAgree() doesn't seem to be implemented, though
-		 */
-		if (pair.getAnaphor().getProperName() && // check if person
-				isAnimate(pair.getAntecedent()) && // check if animate 
-				!isNeutral(pair.getAntecedent())) { // check if neutral
-			return true;
-		}
-		return false;
-	}
-
-	private boolean isAnimate(Mention mention) {
-		/**
-		 * Animacy is set using:
-		 * (a) a static list for pronouns;
-		 * (b) NER labels (e.g., PERSON is animate whereas LOCATION is not); and
-		 * (c) a dictionary bootstrapped from the Web (Ji and Lin 2009)
-		 */
-		// (a) check with pronoun list
-		// not necessary for isRoleAppositive, maybe for other applications
-		String[] tokens = mention.getMarkable().getDiscourseElements();
-		for (int i = 0; i < tokens.length; i++) {
-			String t = tokens[i].toLowerCase();
-			if (t.matches(MALE_PRONOUN_ADJ) || t.matches(FEMALE_PRONOUN_ADJ) ||
-					t.matches(FIRST_PERSON_SG_PRO) || t.matches(FIRST_PERSON_PL_PRO) ||
-					t.matches(SECOND_PERSON_PRO)) {
-				return true;
-			}
-		}
-		// (b) check with NER labels
-		if (SemanticClass.isaPerson(mention.getSemanticClass())) {
-			return true;
-		}
-		else if (SemanticClass.isaObject(mention.getSemanticClass()) ||
-				SemanticClass.isaNumeric(mention.getSemanticClass())) {
-			return false;
-		}
-		// (c) check with bootstrapped dictionary
-		for (int i = 0; i < tokens.length; i++) {
-			/* at the moment, one token suffices to be animate or inanimate to
-			 * arrive at a decision; maybe only consider the head word? 
-			 */
-			if (langPlugin.isInAnimateList(tokens[i])) {
-				return true;
-			}
-			else if (langPlugin.isInInanimateList(tokens[i])) {
-				return false;
-			}
-		}		
-		return false;
-	}
-	
-	private boolean isNeutral(Mention mention) {
-		
-		if (SemanticClass.isaPerson(mention.getSemanticClass())) {
-			return false;
-		}
-		String[] tokens = mention.getMarkable().getDiscourseElements();
-			
-		for (int i = 0; i < tokens.length; i++) {
-			String t = tokens[i];
-			// same comment as loop above
-			if (langPlugin.isInNeutralList(t)) {
-				return true;
-			}
-			else if (langPlugin.isInMaleList(t) || langPlugin.isInFemaleList(t)) {
-				return false;
-			}
-		}
-		return false;
-	}
-
-	private boolean isRelativePronoun(PairInstance pair) {
-		
-		String[] tokens = pair.getAnaphor().getMarkable().getDiscourseElements();
-		if (tokens.length == 1 && tokens[0].matches(RELATIVE_PRONOUN)) {
-			// check if modifies head of antecedent NP
-			EnglishLanguagePlugin.getHead(null)
-		}
-		return false;
-	}
-	
-	private boolean isAcronym(PairInstance pair) {
-		/**
-		 * Checks if one mention is an acronym of the other mention
-		 * and vice versa.
-		 */
-		String mention = pair.getAnaphor().toString();
-		String antecedent = pair.getAntecedent().toString();
-		return checkOneWayAcronym(mention, antecedent) || checkOneWayAcronym(antecedent, mention);
-	}
-	
-	private boolean checkOneWayAcronym(String acronym, String expression) {
-		/**
-		 * Checks if one string is an acronym of the other string
-		 */
-		if (acronym.toUpperCase().equals(acronym)) {
-			String initials = "";
-			for (String word : expression.split(" ")) {
-				initials += word.substring(0,1).toUpperCase();
-			}
-			if (acronym.equals(initials)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private boolean isDemonym(PairInstance pair) {
-		/**
-		 * Check if one expression is a demonym of the other using
-		 * a static list of countries and their gentilic forms from
-		 * Wikipedia
-		 */
-		String mention = pair.getAnaphor().toString();
-		String antecedent = pair.getAntecedent().toString();
-		String mention_lookup = langPlugin.lookupAlias(mention, TableName.DemonymMap);
-		String antecedent_lookup = langPlugin.lookupAlias(antecedent, TableName.DemonymMap);
-		
-		if ((mention_lookup != null && mention_lookup.equals(antecedent)) ||
-				(antecedent_lookup != null && antecedent_lookup.equals(mention))) {
-			return true;
-		}
-		return false;		
-	}		
 }
---
+++ BART/src/elkfed/coref/algorithms/sieve/SieveUtilities.java
@@ -0,0 +1,182 @@
+package elkfed.coref.algorithms.sieve;
+
+import static elkfed.lang.EnglishLinguisticConstants.FEMALE_PRONOUN_ADJ;
+import static elkfed.lang.EnglishLinguisticConstants.FIRST_PERSON_PL_PRO;
+import static elkfed.lang.EnglishLinguisticConstants.FIRST_PERSON_SG_PRO;
+import static elkfed.lang.EnglishLinguisticConstants.MALE_PRONOUN_ADJ;
+import static elkfed.lang.EnglishLinguisticConstants.RELATIVE_PRONOUN;
+import static elkfed.lang.EnglishLinguisticConstants.SECOND_PERSON_PRO;
+import elkfed.config.ConfigProperties;
+import elkfed.coref.PairInstance;
+import elkfed.coref.features.pairs.FE_AppositiveParse;
+import elkfed.coref.features.pairs.FE_Copula;
+import elkfed.coref.mentions.Mention;
+import elkfed.knowledge.SemanticClass;
+import elkfed.lang.LanguagePlugin;
+import elkfed.lang.LanguagePlugin.TableName;
+
+/**
+ * Utilitiy class for sieves
+ * Sieves using these methods:
+ * 	- PreciseConstructsSieve
+ * 
+ * @author Sebastian
+ *
+ */
+
+public class SieveUtilities {
+	
+	private static final LanguagePlugin langPlugin = ConfigProperties.getInstance().getLanguagePlugin();
+	
+	boolean isAppositive(PairInstance pair) {
+		/*
+		 * there is FE_AppositiveParse.getAppositivePrs(pair) as well
+		 * might have to investigate how they differ
+		 * 
+		 * appositive constructions as one NP in TüBa-D/Z
+		 * switch on and off
+		 * 
+		 * there is also FE_Appositive.getAppositive(pair)
+		 */
+		if (FE_AppositiveParse.getAppositivePrs(pair)) {
+			return true;
+		}
+		return false;
+	}
+
+	boolean isPredicateNominative(PairInstance pair) {
+		if (FE_Copula.getCopula(pair)){
+			return true;
+		}
+		return false;
+	}		
+	
+	boolean isRoleAppositive(PairInstance pair) {
+		/*
+		 * there is FE_AnimacyAgree
+		 * FE_AnimacyAgree() doesn't seem to be implemented, though
+		 */
+		if (pair.getAnaphor().getProperName() && // check if person
+				isAnimate(pair.getAntecedent()) && // check if animate 
+				!isNeutral(pair.getAntecedent())) { // check if neutral
+			return true;
+		}
+		return false;
+	}
+
+	boolean isAnimate(Mention mention) {
+		/**
+		 * Animacy is set using:
+		 * (a) a static list for pronouns;
+		 * (b) NER labels (e.g., PERSON is animate whereas LOCATION is not); and
+		 * (c) a dictionary bootstrapped from the Web (Ji and Lin 2009)
+		 */
+		// (a) check with pronoun list
+		// not necessary for isRoleAppositive, maybe for other applications
+		String[] tokens = mention.getMarkable().getDiscourseElements();
+		for (int i = 0; i < tokens.length; i++) {
+			String t = tokens[i].toLowerCase();
+			if (t.matches(MALE_PRONOUN_ADJ) || t.matches(FEMALE_PRONOUN_ADJ) ||
+					t.matches(FIRST_PERSON_SG_PRO) || t.matches(FIRST_PERSON_PL_PRO) ||
+					t.matches(SECOND_PERSON_PRO)) {
+				return true;
+			}
+		}
+		// (b) check with NER labels
+		if (SemanticClass.isaPerson(mention.getSemanticClass())) {
+			return true;
+		}
+		else if (SemanticClass.isaObject(mention.getSemanticClass()) ||
+				SemanticClass.isaNumeric(mention.getSemanticClass())) {
+			return false;
+		}
+		// (c) check with bootstrapped dictionary
+		for (int i = 0; i < tokens.length; i++) {
+			/* at the moment, one token suffices to be animate or inanimate to
+			 * arrive at a decision; maybe only consider the head word? 
+			 */
+			if (langPlugin.isInAnimateList(tokens[i])) {
+				return true;
+			}
+			else if (langPlugin.isInInanimateList(tokens[i])) {
+				return false;
+			}
+		}		
+		return false;
+	}
+	
+	boolean isNeutral(Mention mention) {
+		
+		if (SemanticClass.isaPerson(mention.getSemanticClass())) {
+			return false;
+		}
+		String[] tokens = mention.getMarkable().getDiscourseElements();
+			
+		for (int i = 0; i < tokens.length; i++) {
+			String t = tokens[i];
+			// same comment as loop above
+			if (langPlugin.isInNeutralList(t)) {
+				return true;
+			}
+			else if (langPlugin.isInMaleList(t) || langPlugin.isInFemaleList(t)) {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	boolean isRelativePronoun(PairInstance pair) {
+		
+		String[] tokens = pair.getAnaphor().getMarkable().getDiscourseElements();
+		if (tokens.length == 1 && tokens[0].matches(RELATIVE_PRONOUN)) {
+			// check if modifies head of antecedent NP
+			// still needs to be implemented
+		}
+		return false;
+	}
+	
+	boolean isAcronym(PairInstance pair) {
+		/**
+		 * Checks if one mention is an acronym of the other mention
+		 * and vice versa.
+		 */
+		String mention = pair.getAnaphor().toString();
+		String antecedent = pair.getAntecedent().toString();
+		return checkOneWayAcronym(mention, antecedent) || checkOneWayAcronym(antecedent, mention);
+	}
+	
+	boolean checkOneWayAcronym(String acronym, String expression) {
+		/**
+		 * Checks if one string is an acronym of the other string
+		 */
+		if (acronym.toUpperCase().equals(acronym)) {
+			String initials = "";
+			for (String word : expression.split(" ")) {
+				initials += word.substring(0,1).toUpperCase();
+			}
+			if (acronym.equals(initials)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	boolean isDemonym(PairInstance pair) {
+		/**
+		 * Check if one expression is a demonym of the other using
+		 * a static list of countries and their gentilic forms from
+		 * Wikipedia
+		 */
+		String mention = pair.getAnaphor().toString();
+		String antecedent = pair.getAntecedent().toString();
+		String mention_lookup = langPlugin.lookupAlias(mention, TableName.DemonymMap);
+		String antecedent_lookup = langPlugin.lookupAlias(antecedent, TableName.DemonymMap);
+		
+		if ((mention_lookup != null && mention_lookup.equals(antecedent)) ||
+				(antecedent_lookup != null && antecedent_lookup.equals(mention))) {
+			return true;
+		}
+		return false;		
+	}		
+
+}
