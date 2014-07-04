d34c2e06c291b8a5c834e3d791f7cf003fb25d34
sebastianruder <ruder.sebastian@gmail.com>
2014-07-04 15:25:34 +0200

    Added coref information to script and corpus

--- BART/tuebadz-MMAX2/xml2txt.py
+++ BART/tuebadz-MMAX2/xml2txt.py
@@ -3,11 +3,12 @@ import xml.etree.ElementTree as et
 with open("mmax-mini-plain.txt", "w") as txt:
     # opens the first three files
     for i in range(1,4):
-        file_name = "mmax-all/Basedata/export_00{0}_words.xml".format(i)
-        markable_file_name = "mmax-all/markables/export_00{0}_markable_level.xml".format(i)
+        file_name = "mmax-mini/Basedata/export_00{0}_words.xml".format(i)
+        markable_file_name = "mmax-mini/markables/export_00{0}_markable_level.xml".format(i)
+        coref_file_name = "mmax-mini/markables/export_00{0}_coref_level.xml".format(i)
         tree = et.parse(file_name)
         
-        # creates a dictionary which looks like this:
+        # creates a markable dictionary which looks like this:
         # markable_dict = {"word_2": {"start_of": ["markable_13"]}, "word_3": {"end_of": ["markable_13"]}...}
         # as markables can be embedded
         markables = et.parse(markable_file_name).getroot()
@@ -26,30 +27,66 @@ with open("mmax-mini-plain.txt", "w") as txt:
             markable_dict[start]["start_of"].append(markable_id)
             markable_dict[end]["end_of"].append(markable_id)
         
+        # creates a coref dictionary which looks like this
+        # coref_dict = {"word_32": {"start_of": ["1:502"]}, "word_34": {"end_of": ["1:502"]}...}
+        corefs = et.parse(coref_file_name).getroot()
+        coref_dict = {}
+        spans = [(coref.get("coref_set"), coref.get("span")) for coref in corefs]
+        for coref_set, span in spans:
+            if ".." in span:
+                start = span.split("..")[0]
+                end = span.split("..")[1]
+            else:
+                start, end = span, span
+            coref_dict.setdefault(start, {})
+            coref_dict.setdefault(end, {})
+            coref_dict[start].setdefault("start_of", [])
+            coref_dict[end].setdefault("end_of", [])
+            coref_dict[start]["start_of"].append(coref_set)
+            coref_dict[end]["end_of"].append(coref_set)
+            
         # writes file name for differentiation
         txt.write("Datei: {0}\n".format(file_name))
         # iterates over all words in words file
         for child in tree.getroot():
             id = child.get("id")
-            start_end_dict = markable_dict.get(id)
-            if start_end_dict:
-                start = start_end_dict.get("start_of")
-                end = start_end_dict.get("end_of")
+            # sets coref start and end if word is start or end of a coref
+            coref_start_end_dict = coref_dict.get(id)
+            if coref_start_end_dict:
+                coref_start = coref_start_end_dict.get("start_of")
+                coref_end = coref_start_end_dict.get("end_of")
             else:
-                start, end = None, None
+                coref_start, coref_end = None, None
+            # sets markable start and end if word is start or end of a markable  
+            markable_start_end_dict = markable_dict.get(id)
+            if markable_start_end_dict:
+                markable_start = markable_start_end_dict.get("start_of")
+                markable_end = markable_start_end_dict.get("end_of")
+            else:
+                markable_start, markable_end = None, None
+            # if the word is the start of a coref, adds start tag
+            if coref_start:
+                for coref_set in coref_start:
+                    print "<{0}>".format(coref_set),
+                    txt.write("<{0}>".format(coref_set))
             # if the word is the start of a markable, adds start tag
-            if start:
-                for markable_id in start:
+            if markable_start:
+                for markable_id in markable_start:
                      print "<{0}>".format(markable_id),
                      txt.write("<{0}>".format(markable_id))
             # writes word
             print "{0}".format(child.text),
             txt.write("{0}".format(child.text))
-            # if the word is end of a markable, adds end tag
-            if end:
-                for markable_id in end:
+            # if the word is the end of a markable, adds end tag
+            if markable_end:
+                for markable_id in markable_end:
                     print "</{0}>".format(markable_id),
                     txt.write("</{0}>".format(markable_id))
+            # if the word is the end of a coref, adds end tag
+            if coref_end:
+                for coref_set in coref_end:
+                    print "</{0}>".format(coref_set),
+                    txt.write("</{0}>".format(coref_set))
             print " ",
             txt.write(" ")
             # puts every sentence on a separate line
