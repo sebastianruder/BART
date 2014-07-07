fc7e4b314ab6961b6b2b408a42f2959a1fb5517a
sebastianruder <ruder.sebastian@gmail.com>
2014-07-07 13:50:39 +0200

    Small changes in RelaxedStringMatchSieve

--- BART/src/elkfed/coref/algorithms/sieve/RelaxedStringMatchSieve.java
+++ BART/src/elkfed/coref/algorithms/sieve/RelaxedStringMatchSieve.java
@@ -11,49 +11,30 @@ import elkfed.coref.mentions.Mention;
  * participial postmodifiers) are identical (e.g., [Clinton]
  * and [Clinton, whose term ends in January]).
  * 
- * IMPORTANT: Mentions identified by BART DON'T contain
- * such post-modifying text.
- * 
- * Relaxed String Match -- in this respect -- is the same
- * as Exact String Match. Ignoring pre-modifiers -- in turn --
- * would make it equal to Strict Head Match.
- * Possibilities:
- * 	- omit sieve
- * 	
- * Mention has parameter _postmodifiers; is always empty, though.
- * 
- * Ask Yannick about this.
- * 
  * @author Sebastian
  *
  */
 
 public class RelaxedStringMatchSieve extends Sieve {
 
-	private List<Mention> potentialAntecedents;
+	private List<Mention> antecedents;
 	private String name;
 	
 	public RelaxedStringMatchSieve(List<Mention> mentions) {
-		this.potentialAntecedents = mentions;
-		this.name = "RelaxedDtringMatchSieve";
-		
+		this.antecedents = mentions;
+		this.name = "RelaxedStringMatchSieve";
 	}
 		
 	public int runSieve(Mention mention){
-		int mention_idx = potentialAntecedents.indexOf(mention);
+		int mention_idx = antecedents.indexOf(mention);
 		int ante_idx = -1;
-		// sentences should be displayed somehow
-		// Markable[] array = mention.getSentenceMarkables("sentence");
 		for (int idx = 0; idx < mention_idx; idx++){
-
-			if (mention.toString().equals(potentialAntecedents.get(idx).toString())){
-				// articles are still matched; needs to be fixed
+			if (mention.toString().equals(antecedents.get(idx).toString())){
 				if (!(mention.getPronoun())) {
 					ante_idx = idx;
 				}
 			}
 		}
-		//System.out.println(potentialAntecedents.get(ante_idx));
 		return ante_idx;
 	}
 
@@ -61,6 +42,4 @@ public class RelaxedStringMatchSieve extends Sieve {
 	public String getName() {
 		return this.name;
 	}
-	
-
 }
