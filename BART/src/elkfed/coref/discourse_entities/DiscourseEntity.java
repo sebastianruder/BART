db5c50e04c15c2f4ff76bb223261f87866d6aedb
julianbaumann <julian_baumann@t-online.de>
2014-08-10 12:57:13 +0200

    cleaned up DiscourseEntity, added some doc comments

--- BART/src/elkfed/coref/discourse_entities/DiscourseEntity.java
+++ BART/src/elkfed/coref/discourse_entities/DiscourseEntity.java
@@ -1,68 +1,46 @@
-/**************************************************************************
- *  GuiTAR - A General Tool for Anaphora Resolution
- *  Copyright (C) 2004-2007
- *  Mijail Kabadjov, Massimo Poesio, Olivia Sanchez-Graillet, Philippe Goux
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- **************************************************************************/
 package elkfed.coref.discourse_entities;
-
-
 
 import elkfed.coref.mentions.*;
 import edu.stanford.nlp.trees.Tree;
 import elkfed.config.ConfigProperties;
 import elkfed.lang.GermanLanguagePlugin;
 import elkfed.lang.LanguagePlugin;
-import elkfed.lang.NodeCategory;
-
 import java.util.*;
-import java.util.logging.Logger;
-
-import elkfed.knowledge.SemanticClass;
 import elkfed.lang.EnglishLanguagePlugin;
-import elkfed.lang.MentionType.Features;
-import elkfed.mmax.minidisc.Markable;
-import elkfed.mmax.minidisc.MarkableLevel;
 import elkfed.nlp.util.Gender;
 import elkfed.nlp.util.Number;
+
 /**
- * @author julianbaumann
+ * A Class that functions as a Discourse Entity where all mentions belonging to the same entity
+ * are jointly modeled. 
+ * <p>
+ * To be used in entity-centric coreference resolution.
+ * 
+ * @author Julian
  */
-
-   /*
-    * minimal DiscourseEntity, should work for StringMatch by iterating over mentions
-    */
-
 public class DiscourseEntity {
-	
+
 	private static final LanguagePlugin langPlugin = ConfigProperties
 			.getInstance().getLanguagePlugin();
-	
+
 	private static int nextID = 0;
 	private int ID;
-	
+
 	private boolean firstMention_isFirstMention;
-	
+
 	private TreeSet<Mention> mentions;
-    private Set<String> words;
-    private Set<String> heads;
-    private Set<Tree> modifiers;
-    private Set<Gender> genders;
-    private Set<Number> numbers; 
-    
+	private Set<String> words;
+	private Set<String> heads;
+	private Set<Tree> modifiers;
+	private Set<Gender> genders;
+	private Set<Number> numbers;
+	
+	/**
+	 * The constructor which creates a DscourseEntity for the given mention.
+	 * Should only be called at mention creation.
+	 * 
+	 * @param the mention for which a DiscourseEntity shall be created
+	 */
 	public DiscourseEntity(Mention m) {
 		ID = nextID;
 		nextID++;
@@ -70,68 +48,69 @@ public class DiscourseEntity {
 		mentions.add(m);
 		words = new HashSet<String>();
 		addWords(m);
-		
+
 		heads = new HashSet<String>();
-		//heads.add(m.getHeadString());
 		heads.add(m.getHeadLemma());
-		modifiers = new HashSet<Tree>();				
-		modifiers.addAll(m._premodifiers);		
+
+		modifiers = new HashSet<Tree>();
+		modifiers.addAll(m._premodifiers);
 		modifiers.addAll(m._postmodifiers);
-		
+
 		genders = new HashSet<Gender>();
-		genders.add(m.getGender());	
-		
+		genders.add(m.getGender());
+
 		numbers = new HashSet<Number>();
 		numbers.add(m.getNumberLabel());
-	}	
+	}
 
-	
 	public TreeSet<Mention> getMentions() {
 		return mentions;
 	}
-	
+
 	public Mention getFirstMention() {
 		return mentions.first();
 	}
-	
-	public void addWords(Mention m) {
-		if (langPlugin instanceof GermanLanguagePlugin) {		
-			for (String word: m.getDiscourseElementsByLevel("lemma")) {
-				if (!langPlugin.isInStopwordList(word)) {
-					words.add(word);
-				}
-			}
-		}
-		
-		else if (langPlugin instanceof EnglishLanguagePlugin) {
-			for (String word: m.getDiscourseElementsByLevel("morph")) {
-				if (!langPlugin.isInStopwordList(word)) {
-					words.add(word);
-				}
-			}
-		}
-	}        
 
-	
+	public void addWords(Mention m) {
+		if (langPlugin instanceof GermanLanguagePlugin) {
+			for (String word : m.getDiscourseElementsByLevel("lemma")) {
+				if (!langPlugin.isInStopwordList(word)) {
+					words.add(word);
+				}
+			}
+		}
+
+		else if (langPlugin instanceof EnglishLanguagePlugin) {
+			for (String word : m.getDiscourseElementsByLevel("morph")) {
+				if (!langPlugin.isInStopwordList(word)) {
+					words.add(word);
+				}
+			}
+		}
+	}
+
 	public Set<Gender> getGenders() {
 		return genders;
 	}
-	
+
 	public void setGender(Gender gender) {
-		genders = new HashSet<Gender>();
 		genders.add(gender);
 	}
-	
+
 	public void addGender(Gender gender) {
 		genders.add(gender);
 	}
-	
+
 	public Set<Number> getNumbers() {
 		return numbers;
 	}
-	
+	/**
+	 * Merges the DiscourseEntity of the given Mention with this DiscourseEntity
+	 * 
+	 * @param the Mention which DiscourseEntity should be merged
+	 */
 	public void merge(Mention ante) {
-		
+
 		DiscourseEntity deAnte = ante.getDiscourseEntity();
 		for (Mention m : deAnte.getMentions()) {
 			mentions.add(m);
@@ -140,45 +119,46 @@ public class DiscourseEntity {
 		}
 		genders.addAll(getGenders());
 		numbers.addAll(getNumbers());
-		words.addAll(deAnte.getWords());		
+		words.addAll(deAnte.getWords());
 		modifiers.addAll(deAnte.getModifiers());
 		heads.addAll(deAnte.getHeads());
 	}
-	
+
 	public Set<Tree> getModifiers() {
 		return modifiers;
 	}
-	
+
 	public String getWordsString() {
 		StringBuilder w = new StringBuilder();
-	    for (String word : words) {
-	    	w.append(word);
-	    	w.append(" ");
-	    }
-	    return w.toString();
+		for (String word : words) {
+			w.append(word);
+			w.append(" ");
+		}
+		return w.toString();
 	}
+
 	public Set<String> getHeads() {
 		return heads;
 	}
-	
+
 	public int getID() {
 		return ID;
 	}
-	
+
 	public Set<String> getWords() {
 		return words;
 	}
-	
+
 	public String getHeadsString() {
 		StringBuilder w = new StringBuilder();
-	    for (String head : heads) {
-	    	w.append(head);
-	    	w.append(" ");	    
-	    }
-	    return w.toString();
+		for (String head : heads) {
+			w.append(head);
+			w.append(" ");
+		}
+		return w.toString();
 	}
-	
+
 	public void set_firstMention_isFirstMention(boolean isFirstMention) {
-	        firstMention_isFirstMention = isFirstMention;
-	    }
+		firstMention_isFirstMention = isFirstMention;
+	}
 }
